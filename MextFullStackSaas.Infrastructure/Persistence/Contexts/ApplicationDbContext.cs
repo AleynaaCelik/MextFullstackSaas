28a0394226549683c3806c17fe4d7ec5e597a559
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-21 10:19:42 +0300

    Adding the Web API Layer

--- MextFullStackSaas.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
+++ MextFullStackSaas.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using MextFullstackSaas.Domain.Entities;
 using MextFullstackSaas.Domain.Identity;
+using MextFullstackSaaS.Domain.Identity;
 using MextFullStackSaas.Application.Common.Interfaces;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,10 +12,22 @@ using System.Threading.Tasks;
 
 namespace MextFullStackSaas.Infrastructure.Persistence.Contexts
 {
-    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>, IApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid,UserClaim,UserRole,UserLogin,RoleClaim,UserToken>, IApplicationDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+        
+        }
         public DbSet<Order> Orders { get; set; }
         public DbSet<UserBalance> UserBalances { get; set; }
         public DbSet<UserBalanceHistory> UserBalanceHistories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
     }
 }
