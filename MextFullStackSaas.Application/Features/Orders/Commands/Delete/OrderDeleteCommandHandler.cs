977e8c272acc5cd0ec5f6f7e8aef01e8b4568516
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-21 14:07:06 +0300

    Creating the necessary folders for Mediatr

---
+++ MextFullStackSaas.Application/Features/Orders/Commands/Delete/OrderDeleteCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using MextFullStackSaas.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullStackSaas.Application.Features.Orders.Commands.Delete
+{
+    public class OrderDeleteCommandHandler : IRequestHandler<OrderDeleteCommand, Guid>
+    {
+
+        private readonly IApplicationDbContext _dbcontext;
+
+        public OrderDeleteCommandHandler(IApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<Guid> Handle(OrderDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var order=await _dbcontext.Orders.FirstOrDefaultAsync(x=>x.Id==request.Id,cancellationToken);
+
+            _dbcontext.Orders.Remove(order);
+            await _dbcontext.SaveChangesAsync(cancellationToken);
+
+            return order.Id;
+        }
+    }
+}
---
+++ MextFullStackSaas.WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MextFullStackSaas.Application.Features.Orders.Commands.Delete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MextFullStackSaas.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly ISender _mediatr;
+        public OrdersController(ISender mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteAsync(Guid id,CancellationToken cancellationToken)
+        {
+            return Ok(await _mediatr.Send(new OrderDeleteCommand(id),cancellationToken));
+        }
+    }
+}
