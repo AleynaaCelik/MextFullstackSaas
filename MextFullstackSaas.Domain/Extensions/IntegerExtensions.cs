be7379d9009fb2d63623f91425b0efd2144eeb4b
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-21 10:29:21 +0300

    Creating extension methods

---
+++ MextFullstackSaas.Domain/Extensions/IntegerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaas.Domain.Extensions
+{
+    public static class IntegerExtensions
+    {
+        //Extensions methodlarını oluşturmak için static olmalı 
+
+
+    }
+}
