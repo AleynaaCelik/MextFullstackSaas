ceabcd6c2812d8bebd150b37acaa1158eee19816
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-22 13:45:14 +0300

    DbContext Configuration

---
+++ MextFullStackSaas.Infrastructure/Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullStackSaas.Infrastructure.Persistence.Contexts
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            //build Configuration
+
+            var configuration=new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder=new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
