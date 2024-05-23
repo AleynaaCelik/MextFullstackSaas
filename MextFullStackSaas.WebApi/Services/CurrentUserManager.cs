f70cc1c56de17505586b111e9b1a3796cd729c1e
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-23 10:23:32 +0300

    Adding AddHttpContextAccessor

---
+++ MextFullStackSaas.WebApi/Services/CurrentUserManager.cs
@@ -0,0 +1,23 @@
+﻿using MextFullStackSaas.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace MextFullStackSaas.WebApi.Services
+{
+    public class CurrentUserManager : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CurrentUserManager(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            
+        }
+
+        public Guid UserId => new Guid("35c16d2a-f25b-4701-9a74-ea1fb7ed6d93");
+
+        private Guid GetUserId() 
+        {
+            var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue("uid");
+            return userId is null ? Guid.Empty:Guid.Parse(userId);
+        }
+    }
+}
