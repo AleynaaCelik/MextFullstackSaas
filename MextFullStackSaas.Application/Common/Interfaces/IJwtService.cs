aae6d1f3da92f2987669846a18ca91e1b136894a
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-24 13:12:35 +0300

    Creating the IjwtService interface

--- MextFullStackSaas.Application/Common/Interfaces/IJwtService.cs
+++ MextFullStackSaas.Application/Common/Interfaces/IJwtService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MextFullstackSaas.Domain.Identity;
+using MextFullStackSaas.Application.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,9 @@ namespace MextFullStackSaas.Application.Common.Interfaces
 {
     public interface IJwtService
     {
+        JwtDto GenareteToken(User user,List<string>roles);
+        Task<JwtDto> GenerateTokenAsync(Guid userId, string email,CancellationToken cancellationToken);
+        //Task<JwtDto> GenareteToken(User user);
 
     }
 }
