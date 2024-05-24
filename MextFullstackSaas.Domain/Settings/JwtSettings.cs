eb44af1ae0a41377ff483b59291aa951eebce0d7
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-24 10:47:06 +0300

    Editing JwtSettings

--- MextFullstackSaas.Domain/Settings/JwtSettings.cs
+++ MextFullstackSaas.Domain/Settings/JwtSettings.cs
@@ -9,14 +9,13 @@ namespace MextFullstackSaas.Domain.Settings
     public class JwtSettings
     {
 
-        public class Rootobject
-        {
+       
             public string SecretKey { get; set; }
             public int AccessTokenExpirationInMinutes { get; set; }
             public int RefreshTokenExpirationDays { get; set; }
             public string Issuer { get; set; }
             public string Audience { get; set; }
-        }
+       
 
     }
 
