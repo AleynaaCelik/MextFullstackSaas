c8eb4ff55c1704c8a79165e8eb722c634a547a8d
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-28 09:44:02 +0300

    Serilog logging

--- MextFullStackSaas.WebApi/Program.cs
+++ MextFullStackSaas.WebApi/Program.cs
@@ -6,43 +6,65 @@ using MextFullStackSaas.WebApi;
 
 using MextFullStackSaas.WebApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection; // Doðru namespace'i ekleyin
+using Microsoft.Extensions.DependencyInjection;
+using Serilog; // Doðru namespace'i ekleyin
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers(opt =>
+try
 {
-    opt.Filters.Add<GlobalExceptionFilter>();
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    Log.Information("Starting web application");//Loglamanýn baþladýðýný 
 
-builder.Services.AddApplication();
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddInfrastructure(builder.Configuration); // IConfiguration nesnesini geçirin
+    builder.Services.AddSerilog(); // <-- Add this line Serilog
 
-builder.Services.AddWebServices(builder.Configuration);
+    // Add services to the container.
 
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
-var app = builder.Build();
+    builder.Services.AddControllers(opt =>
+    {
+        opt.Filters.Add<GlobalExceptionFilter>();
+    });
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddApplication();
+
+    builder.Services.AddInfrastructure(builder.Configuration); // IConfiguration nesnesini geçirin
+
+    builder.Services.AddWebServices(builder.Configuration);
+
+    builder.Services.Configure<ApiBehaviorOptions>(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception e)
+{
+   
+    Log.Fatal(e, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();//temizle 
+}
+//Finally try da olsa catch olsa hepsinin sonunda çalýþýr 
