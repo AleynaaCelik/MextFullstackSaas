54f8f2e6952db7b255c9555a365a6684090126ae
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-06-14 13:45:00 +0300

    Casching transcitions

---
+++ MextFullStackSaas.Application/Common/Helpers/MemoryCacheHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullStackSaas.Application.Common.Helpers
+{
+    public static class MemoryCacheHelper
+    {
+        public static string OrdersGetAllKey = "OrdersGetAllList";
+        public static string OrdersGetByIdKey(Guid id) => $"OrdersGetById_{id}";
+        public static MemoryCacheEntryOptions GetMemoryCacheEntryOptions() 
+        {
+            return new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(12)).SetPriority(CacheItemPriority.Normal);
+           
+            //12 Saat sonra cashceden silincek
+        }
+    }
+}
--- MextFullStackSaas.Application/Features/Orders/Queries/GetAll/OrderGetAllQueryHandler.cs
+++ MextFullStackSaas.Application/Features/Orders/Queries/GetAll/OrderGetAllQueryHandler.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
+using MextFullStackSaas.Application.Common.Helpers;
 using MextFullStackSaas.Application.Common.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,20 +15,26 @@ namespace MextFullStackSaas.Application.Features.Orders.Queries.GetAll
     {
         private readonly ICurrentUserService _currentUserService;
         private readonly IApplicationDbContext _applicationDbContext;
-
+        private readonly IMemoryCache _memoryCache;
+         
         public OrderGetAllQueryHandler(ICurrentUserService currentUserService, IApplicationDbContext applicationDbContext)
         {
             _currentUserService = currentUserService;
             _applicationDbContext = applicationDbContext;
         }
 
-        public Task<List<OrderGetAllDto>> Handle(OrderGetAllQuery request, CancellationToken cancellationToken)
+        public async Task<List<OrderGetAllDto>> Handle(OrderGetAllQuery request, CancellationToken cancellationToken)
         {
-            return _applicationDbContext
+            List<OrderGetAllDto> orders;
+            if (_memoryCache.TryGetValue(MemoryCacheHelper.OrdersGetAllKey,out orders)) return orders;
+               
+            
+            orders=await _applicationDbContext
                  .Orders
                  .Where(x => x.UserId == _currentUserService.UserId)
                  .Select(o => OrderGetAllDto.FromOrder(o))
                  .ToListAsync(cancellationToken);
+            return orders;
         }
     }
 }
