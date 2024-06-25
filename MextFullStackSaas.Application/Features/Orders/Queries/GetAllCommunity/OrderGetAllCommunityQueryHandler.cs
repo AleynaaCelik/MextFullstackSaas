ddeb823823bd18916aa7a05aa6618c3f00829ecd
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-06-25 10:48:17 +0300

    OrderGetAllCommunity Added

---
+++ MextFullStackSaas.Application/Features/Orders/Queries/GetAllCommunity/OrderGetAllCommunityQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MextFullStackSaas.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullStackSaas.Application.Features.Orders.Queries.GetAllCommunity
+{
+    public class OrderGetAllCommunityQueryHandler:IRequestHandler<OrderGetAllCommunityQuery,List<string>>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public Task<List<string>> Handle(OrderGetAllCommunityQuery request, CancellationToken cancellationToken)
+        {
+            return _applicationDbContext.Orders.AsNoTracking().SelectMany(x=>x.Urls).ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ MextFullStackSaas.WebApi/Hubs/OrderHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MextFullStackSaas.WebApi.Hubs
+{
+    public class OrderHub:Hub
+    {
+        //Frontendin backendde tetikleme yapacağı methodlar
+
+        public async Task NewOrderAddedAsync()
+        {
+
+        }
+        public async Task GetAllOrderForCommunityAsync()
+        {
+
+        }
+
+
+    }
+}
