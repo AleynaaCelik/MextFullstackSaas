7ea09d35ba506b2a6c7d79b70b4b39e63bc8e370
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-06-27 11:05:40 +0300

    googleAutherization Controller  operations

--- MextFullStackSaas.WebApi/Controllers/UsersAuthController.cs
+++ MextFullStackSaas.WebApi/Controllers/UsersAuthController.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using MextFullstackSaas.Domain.Settings;
 using MextFullStackSaas.Application.Common.Translations;
 using MextFullStackSaas.Application.Features.UserAuth.Commands.ForgotPassword;
 using MextFullStackSaas.Application.Features.UserAuth.Commands.Login;
@@ -8,6 +9,7 @@ using MextFullStackSaas.Application.Features.UserAuth.Commands.VerifiyEmail;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 
 namespace MextFullStackSaas.WebApi.Controllers
 {
@@ -15,27 +17,42 @@ namespace MextFullStackSaas.WebApi.Controllers
     [ApiController]
     public class UsersAuthController : ControllerBase
     {
-
         private readonly ISender _mediatr;
-        
-        public UsersAuthController(ISender mediatr)
+        private readonly GoogleSettings _googleSettings;
+        private const string RedirectUri = "https://localhost:7030/api/UsersAuth/signin-google";
+        private readonly string _googleAutherizationUrl;
+
+        public UsersAuthController(ISender mediatr, IOptions<GoogleSettings> googleSettings)
         {
             _mediatr = mediatr;
-            
+            _googleSettings = googleSettings.Value;
+            _googleAutherizationUrl = $"https://accounts.google.com/o/oauth2/v2/auth?" +
+                                         $"client_id={_googleSettings.ClientId}&" +
+                                         $"response_type=code&" +
+                                         $"scope=openid%20email%20profile&" +
+                                         $"access_type=offline&" +
+                                         $"redirect_uri={RedirectUri}";
         }
 
+        [HttpGet("google-signin-start")]
+        public IActionResult GoogleSignInStart()
+        => Redirect(_googleAutherizationUrl);
+        
+
         [HttpPost("register")]
-        public async Task<IActionResult>RegisterAsync(UserAuthRegisterCommand command,CancellationToken cancellationToken)
+        public async Task<IActionResult> RegisterAsync(UserAuthRegisterCommand command, CancellationToken cancellationToken)
         {
-           // throw new ArgumentNullException(command.FirstName, "First name is required");
-           return Ok(await _mediatr.Send(command, cancellationToken));
+            // throw new ArgumentNullException(command.FirstName, "First name is required");
+            return Ok(await _mediatr.Send(command, cancellationToken));
         }
+
         [HttpPost("Login")]
         public async Task<IActionResult> LoginAsync(UserAuthLoginCommand command, CancellationToken cancellationToken)
         {
             // throw new ArgumentNullException(command.FirstName, "First name is required");
             return Ok(await _mediatr.Send(command, cancellationToken));
         }
+
         [HttpGet("verify-email")]
         public async Task<IActionResult> VerifyEmailAsync([FromQuery] UserAuthVerifyEmailCommand command, CancellationToken cancellationToken)
         {
@@ -43,18 +60,19 @@ namespace MextFullStackSaas.WebApi.Controllers
             // throw new ArgumentNullException(command.FirstName, "First name is required");
             return Ok(await _mediatr.Send(command, cancellationToken));
         }
+
         [HttpPost("forgot-password")]
         public async Task<IActionResult> ForgotPasswordAsync(string email, CancellationToken cancellationToken)
         {
             var command = new ForgotPasswordCommand { Email = email };
             return Ok(await _mediatr.Send(command, cancellationToken));
         }
+
         [HttpPost("reset-password")]
         public async Task<IActionResult> ResetPasswordAsync(string email, string token, string newPassword, CancellationToken cancellationToken)
         {
             var command = new ResetPasswordCommand { Email = email, Token = token, NewPassword = newPassword };
             return Ok(await _mediatr.Send(command, cancellationToken));
         }
-
     }
 }
