d2672a727077a782c91ac1c6b7f4ac0e37a23691
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-07-05 10:08:51 +0300

    Currency Operations

---
+++ MextFullstackSaas.Domain/Entities/UserPayment.cs
@@ -0,0 +1,19 @@
+﻿using MextFullstackSaas.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextFullstackSaas.Domain.Entities
+{
+    public class UserPayment:EntityBase<Guid>
+    {
+        public string ConversationId { get; set; }
+        public string BasketId { get; set; }
+        public string Token { get; set; }
+        public decimal Price { get; set; }
+        public decimal PaidPrice { get; set; }
+       
+    }
+}
